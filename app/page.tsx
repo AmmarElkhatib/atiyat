0e4935cd975a9b2df69d222d31822a1c6dc7b51a
AmmarElkhatib <105842950+AmmarElkhatib@users.noreply.github.com>
2024-11-03 21:28:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
+
 import Main from '@/app/components'
-import { useRouter } from 'next/router'
 
 const App = () => {
-  const router = useRouter()
-  const { query } = router
-
-  // يمكنك الآن تمرير query كـ props إلى الـ component Main
   return (
-    <Main query={query} />
+    <Main />
   )
 }
 
